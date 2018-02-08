17662740e2825e14a82f1677e6176a039a8a8719
erinzhangxi <erincheung1101@gmail.com>
2018-02-08 15:59:15 -0500

    temporary login screen

--- SweatIt/src/components/Login.js
+++ SweatIt/src/components/Login.js
@@ -1,8 +1,79 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react'
+import { Image, View, Text, StyleSheet, KeyboardAvoidingView} from 'react-native'
+import LoginForm from './LoginForm';
 
 class Login extends Component {
   render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require('../images/logo.png')}/>
+            <Text style={styles.title}Sweat It/>
+        </View>
+
+
+        <View style={styles.formContainer}>
+          <LoginForm />
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#3498db'
+  },
+  logoContainer: {
+    alignItems:'center',
+    flexGrow:1,
+    justifyContent:'center'
+  },
+  logo: {
+    width:100,
+    height:100
+  },
+  title: {
+    color: '#FFF',
+    marginTop:10,
+    width: 160,
+    textAlign:'center',
+    opacity:0.9
+  }
+});
+  /*
+  static propTypes = {
+   member: PropTypes.shape({
+     email: PropTypes.string,
+   }),
+   error: PropTypes.string,
+   loading: PropTypes.bool.isRequired,
+   onFormSubmit: PropTypes.func.isRequired,
+ }
+
+ static defaultProps = {
+    error: null,
+    member: {},
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: (props.member && props.member.email) ? props.member.email : '',
+      password: '',
+    };
+
+
+  }
+  render() {
+    const { loading, error } = this.props;
+
+    // Loading
+    if (loading) return <Loading />;
+
     return (
       <View style={styles.container}>
           <Text>Login</Text>
@@ -11,6 +82,7 @@ class Login extends Component {
   }
 }
 
+
 const styles=StyleSheet.create( {
   container: {
     flex:1,
@@ -18,6 +90,8 @@ const styles=StyleSheet.create( {
     alignItems:'center',
     backgroundColor:'#2c3e50',
   },
-});
+});*/
+
+
 
 export default Login;
---
+++ SweatIt/src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import { View, StyleSheet, TextInput, Text, TouchableOpacity,
+  KeyboardAvoidingView, StatusBar} from 'react-native'
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <StatusBar
+      barStyle="light-content">
+       </StatusBar>
+        <TextInput
+          placeholder="username or email"
+          placeholderTextColor="rgba(255,255,255, 0.7)"
+          returnKeyType="next"
+          onSubmitEditing = {() => this.passwordInput.focus()}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="password"
+          placeholderTextColor="rgba(255,255,255, 0.7)"
+          returnKeyType="go"
+          secureTextEntry
+          style={styles.input}
+          ref={(input)=>this.passwordInput = input}
+        />
+
+        <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  container: {
+    padding:20
+  },
+  input: {
+    height:40,
+    backgroundColor: 'rgba(255,255,255, 0.2)',
+    marginBottom:10,
+    color: '#fff',
+    paddingHorizontal:10
+  },
+  buttonContainer: {
+      backgroundColor: '#2980b9',
+      paddingVertical: 15
+
+  },
+  buttonText: {
+    textAlign:'center',
+    color: "#FFFFFF",
+    fontWeight: '700'
+  }
+});
