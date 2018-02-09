af69011347e07b89e39242cec5270664621f5dd4
erinzhangxi <erincheung1101@gmail.com>
2018-02-08 21:52:27 -0500

    clean firebase settings

--- SweatIt/index.ios.js
+++ SweatIt/index.ios.js
@@ -1,4 +1,14 @@
 import {AppRegistry} from 'react-native';
 import App from './src/components/App';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCt8VnDKJN6FssJZJqEeA8VYodGQl5BtVE",
+  authDomain: "workout-journal-ez96.firebaseapp.com",
+  databaseURL: "https://workout-journal-ez96.firebaseio.com",
+  storageBucket: "workout-journal-ez96.appspot.com",,
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 AppRegistry.registerComponent('SweatIt', () => App);
